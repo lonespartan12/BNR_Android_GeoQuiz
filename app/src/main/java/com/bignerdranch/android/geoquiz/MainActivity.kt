1a8720c52b93e9f213ae4407a3505ae72d965635
lonespartan12 <lonespartan12@hotmail.com>
2022-06-06 14:25:17 -0600

    comments
    added comments

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -7,21 +7,30 @@ import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var trueButton: Button
+    private lateinit var trueButton: Button //lateint is used when we decalre a variable but do not assign it a value.
     private lateinit var falseButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
+        findViewById is a method that we will be using a lot. the R class that is created on compile, is a a file that adds all the xml tags as objects/methods/classes that we can refer to in the kotlin
+         */
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener { view: View ->
+        /* the listener is a class that constantly looks for an input event associated with the button object
+        This code is written as a lambda function
+            lambda functions are unnamed functions
+        This lambda function takes the view and associates it with the view of the button object.
+         */
+        //trueButton.setOnClickListener { view: View -> //we do not need to use the first view. we can replace this with a underscore since this is an anonymous function
+        trueButton.setOnClickListener { _: View ->//we can use the underscore here since this is an anonymous function. we can do this because we are creating a new instance of View every time the button is clicked
             Toast.makeText(this,R.string.correct_toast, Toast.LENGTH_SHORT).show()
         }
 
-        falseButton.setOnClickListener { view: View ->
+        falseButton.setOnClickListener { view: View ->// we will leave this one as view:View instead of _: View.
             Toast.makeText(this,R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
         }
     }
